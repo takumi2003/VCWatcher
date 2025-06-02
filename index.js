80f5b719e338c4b360514be0281f440394760fb5
takumi2003 <takumi20030819@gmail.com>
2025-06-02 20:50:41 +0900

    feat: 基本機能完成

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [Partials.Channel],
+});
+
+const TOKEN = process.env.DISCORD_TOKEN;
+const NOTIFY_CHANNEL_ID = process.env.NOTIFY_CHANNEL_ID;
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    // ユーザーがVCに入ったときだけ処理する
+    if (!oldState.channel && newState.channel) {
+      const username = newState.member.user.username;
+      const vc = newState.channel;
+      const memberCount = vc.members.size;
+  
+      if (memberCount >= 2) {
+        const notifyChannel = newState.guild.channels.cache.get(NOTIFY_CHANNEL_ID);
+        if (notifyChannel) {
+          notifyChannel.send(`${username} さんが「${vc.name}」に参加しました！（現在 ${memberCount} 人）`);
+        } else {
+          console.log('notifyChannel not found!');
+        }
+      } else {
+        console.log(`${username} が VC に入ったが、まだ1人目なので通知なし`);
+      }
+    }
+  });
+
+client.login(TOKEN);
